37177c996bb47a61c6faeafbe25c3e532097295a
Arka Halder <arkachego25@Arkas-iMac.local>
2025-04-04 01:13:23 +0530

    removed next.js canary version

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   experimental: {
-    nodeMiddleware: true,
     optimizePackageImports: [
       "@mantine/core",
       "@mantine/hooks",
